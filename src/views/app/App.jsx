c140aa32ffd955da23faf5c1fc6ce9ea756070f9
zcallan <kaln.delbridge@hotmail.com>
2017-08-21 10:20:39 +1000

    init

---
+++ src/views/app/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Home } from 'views';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
